88492cff87a2fb6bcb2ddcfeeb1b01e5053cc716
ankita-kashid <56724557+ankita-kashid@users.noreply.github.com>
2022-05-26 23:20:01 +0530

    Create minimum-cost-to-move-chips-to-the-same-position.py

---
+++ Array/minimum-cost-to-move-chips-to-the-same-position.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/minimum-cost-to-move-chips-to-the-same-position/
+
+class Solution:
+    def minCostToMoveChips(self, position: List[int]) -> int:
+        e=0
+        o=0
+        for i in position:
+            if i%2==0:
+                e+=1
+            else:
+                o+=1
+        return min(e,o)
+        
